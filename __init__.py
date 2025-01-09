14d6367df41c34922e6f6c9d90d2f4d0677ad72a
Aurélien Balavoine <132837748+Sh3z4m@users.noreply.github.com>
2025-01-09 12:34:43 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,7 +49,7 @@ def authentification_recherche():
         if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
             session['authentifierecherche'] = True
             # Rediriger vers la route lecture après une authentification réussie
-            return redirect(url_for('/fiche_nom/'))
+            return redirect(url_for('ReadBDD_2'))
         else:
             # Afficher un message d'erreur si les identifiants sont incorrects
             return render_template('formulaire_authentification_recherche.html', error=True)
