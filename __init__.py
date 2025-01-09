443aba63489e1cfa355274ec99eea8cc7cd6f03e
Aurélien Balavoine <132837748+Sh3z4m@users.noreply.github.com>
2025-01-09 12:29:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,6 +12,9 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 def est_authentifie():
     return session.get('authentifie')
 
+def est_authentifierecherche():
+    return session.get('authentifierecherche')
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
